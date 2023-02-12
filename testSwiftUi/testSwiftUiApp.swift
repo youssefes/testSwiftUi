570a7f3bf9ceaf0faa64a5114df4c348fa27524f
Abdulrahman Alaa <46077105+youssefes@users.noreply.github.com>
2023-02-12 16:53:44 +0200

    Initial Commit

---
+++ testSwiftUi/testSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftUiApp.swift
+//  testSwiftUi
+//
+//  Created by Amr Hassan on 12/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
